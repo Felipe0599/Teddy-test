4b58980f53afabead6752789fd27f05b3632b79a
Felipe Moraes <80363113+Felipe0599@users.noreply.github.com>
2024-08-16 17:42:43 -0300

    Create security_groups.tf

---
+++ terraform/security_groups.tf
@@ -0,0 +1,51 @@
+# security_groups.tf
+resource "aws_security_group" "public_sg" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Permitir acesso SSH de qualquer lugar
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Permitir acesso HTTP de qualquer lugar
+  }
+
+  tags = {
+    Name = "my-public-sg"
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]  # Permitir comunicação interna
+  }
+
+  tags = {
+    Name = "my-private-sg"
+  }
+}
